bcc387205c0c9de5cea32b5b5a5dcbac6f12851a
github username <emilyn0614@gmail.com>
2021-06-03 23:32:19 -0500

    user auth login complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < ActionController::API
     end
 
     def current_user
-        # byebug
         if decoded_token
             user_id = decoded_token[0]['user_id']
             user = User.find_by(id: user_id)
